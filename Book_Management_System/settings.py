9196bd2f84ab0ad6e94b920383b261939feb2f1d
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-03-27 17:33:11 +0600

    forth commit

--- Book_Management_System/settings.py
+++ Book_Management_System/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-v_!dwzqx4ke*axdx_t63uxcw49hs^g5@k1#vb88uk39c)(u)5u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
